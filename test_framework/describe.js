93090d0a3af60a1b45b752feaae47f6a31cc0cce
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-10-20 17:24:23 +0100

    modified desrcibe.js

--- test_framework/describe.js
+++ test_framework/describe.js
@@ -1,4 +1,8 @@
-var describe = function(message, callback) {
-    console.log(message);
-    callback();
-}
+(function(exports) {
+    var describe = function(message, callback) {
+        console.log(message);
+        callback();
+    }
+
+    exports.describe = describe;
+})(this);
